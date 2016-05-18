86cd88d65d9261f82b93e13065be0a246afabee3
Dmytro Volovnenko <dmitry_volovnenko@yahoo.com>
2016-05-18 14:04:24 +0300

    deskentry.c: indexOfKey() function added, entry keys array added

---
+++ deskentry.c
@@ -0,0 +1,62 @@
+/*
+*   Copyright (C) 2016 Dmytro Volovnenko
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "deskentry.h"
+
+//----------------------------------------------------------------
+const char *entryKeys[] = {  "Type",
+                              "Version",
+                              "Name",
+                              "GenericName",
+                              "NoDisplay",
+                              "Comment",
+                              "Icon",
+                              "Hidden",
+                              "OnlyShowIn",
+                              "NotShowIn",
+                              "DBusActivatable",
+                              "TryExec",
+                              "Exec",
+                              "Path",
+                              "Terminal",
+                              "Actions",
+                              "MimeType",
+                              "Categories",
+                              "Implements",
+                              "Keywords",
+                              "StartupNotify",
+                              "StartupWMClass",
+                              "URL"
+                           };
+//----------------------------------------------------------------
+
+int writeFullStr(int fd, const char *sourceStr, size_t lenth);
+//----------------------------------------------------------------
+//function takes char *key and return index of this key in const array
+//if have no key return -1
+int indexOfKey(const char *key)
+{
+    int index = -1;
+    int i = 0;
+    for (; i < NUM_OF_KEYS; ++i)
+    {
+        if (!strcmp(key, entryKeys[i]))
+        {
+            index = i;
+            break;
+        }
+    }
+
+    return index;
+}
